b16a023f9315959bec1b17fad0c5cec3182ebbf1
dev-Lesser <dev.jongmoon@gmail.com>
2022-01-08 13:00:00 +0900

    :art: Add create review card box

--- healstudio-community/src/components/ReviewTable.vue
+++ healstudio-community/src/components/ReviewTable.vue
@@ -71,7 +71,57 @@
 
         </div>
         <v-divider />
-        <v-btn > <v-icon class="mr-2" small >mdi-pencil</v-icon>리뷰 작성하기</v-btn>
+        <v-btn @click="overlay = !overlay"> <v-icon class="mr-2" small >mdi-pencil</v-icon>리뷰 작성하기</v-btn>
+        <v-overlay :value="overlay" light  >
+            <v-card class="ma-3 pa-3" :width="500"
+            :height="400" light>
+                <v-card-title>
+                    리뷰
+                    <v-card-actions>
+                        <div><v-chip outlined>{{metaData.name}}</v-chip></div>
+                    </v-card-actions>
+                </v-card-title>
+                <v-divider />
+                    
+                    <v-card-actions>
+                        <div>
+                            평점 : 
+                        </div>
+                        <v-rating
+                            v-model="point"
+                            background-color="grey"
+                            hover
+                            length="5"
+                            color="yellow accent-4"
+                            dense
+                            half-increments
+                            size="25"
+                            ></v-rating> 
+                        <v-spacer />
+                        <div>{{point}} 점</div>
+                    </v-card-actions>
+                    
+                
+                
+                
+                <v-divider class="mt-6" />
+                <v-textarea
+                v-model="contents"
+                clearable	
+                outlined
+                dense
+                class="ma-1 pa-2"
+                label="한줄평 50자"
+                height=100
+                color="grey"
+                ></v-textarea>
+                <v-card-actions>
+                    <v-btn @click="createReview">작성하기</v-btn>
+                    <v-spacer/>
+                    <v-btn @click="overlay = !overlay">취소</v-btn>
+                </v-card-actions>
+            </v-card>
+        </v-overlay>
         </v-list>
         <v-list
             two-line
@@ -99,6 +149,14 @@ export default {
             listWidth: window.innerWidth - 602,
             listHeight: Math.round((window.innerHeight - 483) / 100) * 100,
             isShown: true,
+            overlay: false,
+            point: 0,
+            contents: '',
+        }
+    },
+    methods:{
+        async createReview(){
+            console.log(this.point,this.contents)
         }
     },
 
